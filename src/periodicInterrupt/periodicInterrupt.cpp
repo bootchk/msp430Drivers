069b08e4607b71252eb603815d2ea58aeee7bce5
Lloyd Konneker <konnekerl@gmail.com>
2026-06-09 15:19:56 -0400

    Fix inaccurate VLO in LPM3

--- src/periodicInterrupt/periodicInterrupt.cpp
+++ src/periodicInterrupt/periodicInterrupt.cpp
@@ -13,22 +13,34 @@ and is active in LPM3.5
 This can conflict when RTC is used for LowPowerTimer or Counter.
 */
 
+/*
+Uses the VLO, which is not accurate
+and can vary by 50% over voltage, temperature, and per chip variations.
+Also, reduced by 15% for LPM3.
 
+Here we don't calibrate, but do account for 15% drop,
+since we are using the RTC to time sleep in LPM3.
+*/
+
+// Nominal 10k * 85%
+#define VLONominalLPM3Freq 8500
+// !!! This must match the symbolic value that sets the VLO divisor: RTCPS__1024
+#define VLOClockDivisor 1024
 
 /* Math from seconds to ticks. 
 
 Use:
-VLO at 12k hz
-divisor on RTC module of 1024 
+VLO at 10k hz nominal.
+divisor on RTC module of 1024.
+The clock runs in cycles, the RTC runs in ticks.
 
 tick per sec == 
-12k cycle per sec / 1024 cycle per tick = 11.7 
+8.5 cycle per sec / 1024 cycle per tick = 8.3
 
 sec per tick ==
-1024 cycle per tick /12k cycles per sec = .0853 
+1024 cycle per tick /8.5k cycles per sec = .12
 
-512 ticks = 44.7 sec
-32 ticks = 2.7 sec
+512 ticks == 62 sec
 
 Note the ticks need not be a power of 2.
 */
@@ -36,7 +48,9 @@ void
 PeriodicInterrupt::initInSeconds(uint16_t seconds)
 {
     // Integer math. Cast to long 32-bit to avoid overflow.
-    PeriodicInterrupt::initInTicks (((long) seconds * 12000) / 1024);
+    // Total clock cycles in period divided by clock divisor => ticks
+    PeriodicInterrupt::initInTicks (((long) seconds * VLONominalLPM3Freq)
+                                     / VLOClockDivisor);
 }
 
 
